afdbb5d033de67822d06d434537b7763068b59c7
StefanBregenzer <37838586+StefanBregenzer@users.noreply.github.com>
2020-11-23 12:33:05 +0100

    Update vui-script.js

--- Task_06/js/vui-script.js
+++ Task_06/js/vui-script.js
@@ -1,12 +1,24 @@
 window.addEventListener("load", function () {
     var artyom = new Artyom();
-    artyom.addCommands({
-        indexes: ["erstelle Aufgabe *"],
+    
+    var commandHello = {
+    indexes:["hello","good morning","hey"], // These spoken words will trigger the execution of the command
+    action:function(){ // Action to be executed when a index match with spoken word
+        artyom.say("Hey buddy ! How are you today?");
+        }
+    };
+    
+    var commandAufgabe = {
+        indexed: ["erstelle Aufgabe *"],
         smart: true,
-        action: function (i, wildcard) {
+        action: function(i,wildcard){
             artyom.say("Neue Aufgabe wird erstellt: " + wildcard);
         }
-    });
+    };
+            
+    
+    artyom.addCommands(commandHello, commandAufgabe);
+    
     function startContinuousArtyom() {
         artyom.fatality();
         setTimeout(function () {
