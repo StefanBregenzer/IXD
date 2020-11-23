16044564a02960e20d4c90113f1430b5f874b3ec
StefanBregenzer <37838586+StefanBregenzer@users.noreply.github.com>
2020-11-23 13:14:26 +0100

    Update vui-script.js

--- Task_06/js/vui-script.js
+++ Task_06/js/vui-script.js
@@ -1,39 +1,48 @@
-window.addEventListener("load", function () {
-    var artyom = new Artyom();
-    
-    var commandHello = {
-    indexes:["hallo","guten morgen","hey"], // These spoken words will trigger the execution of the command
-    action:function(){ // Action to be executed when a index match with spoken word
-        artyom.say("Hallo ! Wie geht es dir heute?");
-        console.log("commandHello");
+var artyom = new Artyom();
+
+var commandGroup = [
+    {
+        description: "Aus Höflichkeit sagt man guten Tag",
+        smart: false,
+        indexes: ["hallo","guten morgen","hey"],
+        action:function(){
+            artyom.say("Hallo ! Wie geht es dir heute?");
+            console.log("commandHello");
         }
-    };
-    
-    var commandAufgabe = {
-        indexed: ["erstelle Aufgabe *","brauche Aufgabe *","erstelle die Aufgabe *"],
+    },
+    {
+        description: "Erstellen einer neuen Aufgabe",
         smart: true,
+        indexed: ["erstelle Aufgabe *","brauche Aufgabe *","erstelle die Aufgabe *"],
         action: function(i,wildcard){
             artyom.say("Die neue Aufgabe " + wildcard + " wurde für dich erstellt");
             console.log("Task " + wildcard + " has been created.");
         }
-    };
-            
-    
-    artyom.addCommands(commandHello, commandAufgabe);
-    
-    function startContinuousArtyom() {
-        artyom.fatality();
-        setTimeout(function () {
-            artyom.initialize({
-                lang: "de-DE",
-                continuous: true,
-                listen: true,
-                interimResults: true,
-                debug: true
-            }).then(function () {
-                console.log("Ready!");
-            });
-        }, 250);
     }
+];
+
+window.addEventListener("load", startup());
+                        
+function startup() {
+    
+    artyom.addCommands(commandGroup);
+    
     startContinuousArtyom();
-});
+};
+    
+function startContinuousArtyom() {
+    artyom.fatality();
+    
+    setTimeout(function () {
+        artyom.initialize({
+            lang: "de-DE",
+            continuous: true,
+            listen: true,
+            interimResults: true,
+            debug: true
+        }).then(function () {
+            console.log("Ready!");
+            artyom.say("Möchtest du etwas bestimmtes?");
+        });
+    }, 250);
+};
